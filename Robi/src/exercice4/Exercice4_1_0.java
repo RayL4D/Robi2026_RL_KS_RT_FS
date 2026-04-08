36cd6d15de6a346e2dc7190935b107b10c0b270c
Kevin <sider.kevin22@gmail.com>
2026-04-08 15:15:12 +0200

    Implémentation de l'exercice 4 (Sélection et exécution des commandes)

--- Robi/src/exercice4/Exercice4_1_0.java
+++ Robi/src/exercice4/Exercice4_1_0.java
@@ -1,96 +1,67 @@
 package exercice4;
 
-// 
-//	(space setColor black)  
-//	(robi setColor yellow) 
-//	(space sleep 2000) 
-//	(space setColor white)  
-//	(space sleep 1000) 	
-//	(robi setColor red)		  
-//	(space sleep 1000)
-//	(robi translate 100 0)
-//	(space sleep 1000)
-//	(robi translate 0 50)
-//	(space sleep 1000)
-//	(robi translate -100 0)
-//	(space sleep 1000)
-//	(robi translate 0 -40) ) 
-//
-
 import java.awt.Dimension;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
+import core.Environment;
+import core.Reference;
+import core.RobiInterpreter;
+import core.commands.SetColor;
+import core.commands.Sleep;
+import core.commands.Translate;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
-import stree.parser.SNode;
-import stree.parser.SParser;
 import tools.Tools;
 
+/**
+ * Exercice 4.1 : Introduction de Environment, Reference et Command.
+ * Dispatch dynamique sans if/else, lecture interactive en console.
+ * Commandes supportees : setColor, sleep, translate.
+ */
 public class Exercice4_1_0 {
-	// Une seule variable d'instance
-	Environment environment = new Environment();
 
-	public Exercice4_1_0() {
-		// space et robi sont temporaires ici
-		GSpace space = new GSpace("Exercice 4", new Dimension(200, 100));
-		GRect robi = new GRect();
+    RobiInterpreter interpreter = new RobiInterpreter();
 
-		space.addElement(robi);
-		space.open();
+    /**
+     * Initialise l'environnement avec un space et un rectangle,
+     * enregistre les commandes et lance la boucle de lecture interactive.
+     */
+    public Exercice4_1_0() {
+        Environment env = interpreter.getEnvironment();
 
-		Reference spaceRef = new Reference(space);
-		Reference robiRef = new Reference(robi);
+        GSpace space = new GSpace("Exercice 4.1", new Dimension(200, 100));
+        GRect robi = new GRect();
+        space.addElement(robi);
+        space.open();
 
-		// Initialisation des references : on leur ajoute les primitives qu'elles
-		// comprenent
-		//
-		// <A VOUS DE CODER>
-		//
+        Reference spaceRef = new Reference(space);
+        Reference robiRef = new Reference(robi);
 
-		// Enrigestrement des references dans l'environement par leur nom
-		environment.addReference("space", spaceRef);
-		environment.addReference("robi", robiRef);
+        spaceRef.addCommand("setColor", new SetColor(interpreter));
+        spaceRef.addCommand("sleep", new Sleep(interpreter));
 
-		this.mainLoop();
-	}
+        robiRef.addCommand("setColor", new SetColor(interpreter));
+        robiRef.addCommand("translate", new Translate(interpreter));
 
-	private void mainLoop() {
-		while (true) {
-			// prompt
-			System.out.print("> ");
-			// lecture d'une serie de s-expressions au clavier (return = fin de la serie)
-			String input = Tools.readKeyboard();
-			// creation du parser
-			SParser<SNode> parser = new SParser<>();
-			// compilation
-			List<SNode> compiled = null;
-			try {
-				compiled = parser.parse(input);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// execution des s-expressions compilees
-			Iterator<SNode> itor = compiled.iterator();
-			while (itor.hasNext()) {
-				this.run((SNode) itor.next());
-			}
-		}
-	}
+        env.addReference("space", spaceRef);
+        env.addReference("robi", robiRef);
 
-	private void run(SNode expr) {
-		// quel est le nom du receiver
-		String receiverName = expr.get(0).contents();
-		// quel est le receiver
-		Reference receiver = environment.getReferenceByName(receiverName);
-		// demande au receiver d'executer la s-expression compilee
-		receiver.run(expr);
-	}
+        this.mainLoop();
+    }
 
-	public static void main(String[] args) {
-		new Exercice4_1_0();
-	}
+    private void mainLoop() {
+        while (true) {
+            System.out.print("> ");
+            String input = Tools.readKeyboard();
+            interpreter.oneShot(input);
+        }
+    }
 
+    /**
+     * Point d'entree de l'exercice 4.1.
+     *
+     * @param args les arguments de la ligne de commande (non utilises)
+     */
+    public static void main(String[] args) {
+        new Exercice4_1_0();
+    }
 }
--- Robi/src/exercice4/Exercice4_2_0.java
+++ Robi/src/exercice4/Exercice4_2_0.java
@@ -1,136 +1,81 @@
 package exercice4;
 
- /*
-	(space setColor black)  
-	(robi setColor yellow) 
-	(space sleep 2000) 
-	(space setColor white)  
-	(space sleep 1000) 	
-	(space add robi (GRect new))
-	(robi setColor green)
-	(robi translate 100 50)
-	(space del robi)
-	(robi setColor red)		  
-	(space sleep 1000)
-	(robi translate 100 0)
-	(space sleep 1000)
-	(robi translate 0 50)
-	(space sleep 1000)
-	(robi translate -100 0)
-	(space sleep 1000)
-	(robi translate 0 -40) ) 
-	
-	
-(space add robi (rect.class new))
-(robi translate 130 50)
-(robi setColor yellow)
-(space add momo (oval.class new))
-(momo setColor red)
-(momo translate 80 80)
-(space add pif (image.class new alien.gif))
-(pif translate 100 0)
-(space add hello (label.class new "Hello world"))
-(hello translate 10 10)
-(hello setColor black)
-
-*/
-
-
 import java.awt.Dimension;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
+import core.Environment;
+import core.Reference;
+import core.RobiInterpreter;
+import core.commands.AddElement;
+import core.commands.DelElement;
+import core.commands.NewElement;
+import core.commands.NewImage;
+import core.commands.NewString;
+import core.commands.SetColor;
+import core.commands.Sleep;
 import graphicLayer.GImage;
 import graphicLayer.GOval;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 import graphicLayer.GString;
-import stree.parser.SNode;
-import stree.parser.SParser;
 import tools.Tools;
 
-
-/*
-class NewElement implements Command {
-	public Reference run(Reference reference, SNode method) {
-		try {
-			@SuppressWarnings("unchecked")
-			GElement e = ((Class<GElement>) reference.getReceiver()).getDeclaredConstructor().newInstance();
-			Reference ref = new Reference(e);
-			ref.addCommand("setColor", new SetColor());
-			ref.addCommand("translate", new Translate());
-			ref.addCommand("setDim", new SetDim());
-			return ref;
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-				| NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-}
-*/
-
+/**
+ * Exercice 4.2 : Ajout de la creation dynamique d'elements graphiques.
+ * Commandes ajoutees : add, del, new (pour Rect, Oval, Image, Label).
+ */
 public class Exercice4_2_0 {
-	// Une seule variable d'instance
-	Environment environment = new Environment();
 
-	public Exercice4_2_0() {
-		GSpace space = new GSpace("Exercice 4", new Dimension(200, 100));
-		space.open();
+    RobiInterpreter interpreter = new RobiInterpreter();
 
-		Reference spaceRef = new Reference(space);
-		Reference rectClassRef = new Reference(GRect.class);
-		Reference ovalClassRef = new Reference(GOval.class);
-		Reference imageClassRef = new Reference(GImage.class);
-		Reference stringClassRef = new Reference(GString.class);
+    /**
+     * Initialise l'environnement avec les classes d'elements graphiques,
+     * enregistre les commandes de creation et lance la boucle interactive.
+     */
+    public Exercice4_2_0() {
+        Environment env = interpreter.getEnvironment();
 
-		spaceRef.addCommand("setColor", new SetColor());
-		spaceRef.addCommand("sleep", new Sleep());
+        GSpace space = new GSpace("Exercice 4.2", new Dimension(200, 100));
+        space.open();
 
-		spaceRef.addCommand("add", new AddElement());
-		spaceRef.addCommand("del", new DelElement());
-		
-		rectClassRef.addCommand("new", new NewElement());
-		ovalClassRef.addCommand("new", new NewElement());
-		imageClassRef.addCommand("new", new NewImage());
-		stringClassRef.addCommand("new", new NewString());
+        Reference spaceRef = new Reference(space);
+        spaceRef.addCommand("setColor", new SetColor(interpreter));
+        spaceRef.addCommand("sleep", new Sleep(interpreter));
+        spaceRef.addCommand("add", new AddElement(env, interpreter));
+        spaceRef.addCommand("del", new DelElement(env));
 
-		environment.addReference("space", spaceRef);
-		environment.addReference("rect.class", rectClassRef);
-		environment.addReference("oval.class", ovalClassRef);
-		environment.addReference("image.class", imageClassRef);
-		environment.addReference("label.class", stringClassRef);
-		
-		this.mainLoop();
-	}
-	
-	private void mainLoop() {
-		while (true) {
-			// prompt
-			System.out.print("> ");
-			// lecture d'une serie de s-expressions au clavier (return = fin de la serie)
-			String input = Tools.readKeyboard();
-			// creation du parser
-			SParser<SNode> parser = new SParser<>();
-			// compilation
-			List<SNode> compiled = null;
-			try {
-				compiled = parser.parse(input);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// execution des s-expressions compilees
-			Iterator<SNode> itor = compiled.iterator();
-			while (itor.hasNext()) {
-				new Interpreter().compute(environment, itor.next());
-			}
-		}
-	}
+        Reference rectClassRef = new Reference(GRect.class);
+        Reference ovalClassRef = new Reference(GOval.class);
+        Reference imageClassRef = new Reference(GImage.class);
+        Reference stringClassRef = new Reference(GString.class);
 
-	public static void main(String[] args) {
-		new Exercice4_2_0();
-	}
+        rectClassRef.addCommand("new", new NewElement(env, interpreter));
+        ovalClassRef.addCommand("new", new NewElement(env, interpreter));
+        imageClassRef.addCommand("new", new NewImage(interpreter));
+        stringClassRef.addCommand("new", new NewString(interpreter));
 
+        env.addReference("space", spaceRef);
+        env.addReference("Rect", rectClassRef);
+        env.addReference("Oval", ovalClassRef);
+        env.addReference("Image", imageClassRef);
+        env.addReference("Label", stringClassRef);
+
+        this.mainLoop();
+    }
+
+    private void mainLoop() {
+        while (true) {
+            System.out.print("> ");
+            String input = Tools.readKeyboard();
+            interpreter.oneShot(input);
+        }
+    }
+
+    /**
+     * Point d'entree de l'exercice 4.2.
+     *
+     * @param args les arguments de la ligne de commande (non utilises)
+     */
+    public static void main(String[] args) {
+        new Exercice4_2_0();
+    }
 }
